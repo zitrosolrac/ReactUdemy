4802060b421e4c1006a77b8603062655865d9382
zitrosolrac <carlosortiz9204@gmail.com>
2019-09-17 10:57:43 -0500

    commit finished with section 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css'; //will contain objects that correspond to the .something in .css
+//transforms css into javascript that we could use within this file 
 import Person from './Person/Person';
 
 class App extends Component {
@@ -45,16 +46,8 @@ class App extends Component {
   }
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid pink',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if ( this.state.showPersons ) {
       persons = (
@@ -70,23 +63,23 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red; //this will be a string having access to the css
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2){
-      classes.push('red'); //classes = ['red']
+      assignedClasses.push( classes.red ); //classes = ['red']
     }
     if(this.state.persons.length <= 1){
-      classes.push('bold'); //classes = ['red', 'bold']
+      assignedClasses.push( classes.bold ); //classes = ['red', 'bold']
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}> 
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
         <button
-          style={style}
+          className={btnClass}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import './Person.css';
+import classes from './Person.css'; //ask vignesh about the reason behind this 
+//think of this classes object as a javascript object which gives you access to the string version of the css class 
+//so you can use a css class in a given component thats the only place the css styles apply 
 
 const person = ( props ) => {
 
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
